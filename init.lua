3b38094d3643ef86fd74d3b0fb408eb270425e8a
Philipp Szechenyi <philipp.szechenyi@cgm.com>
2026-02-20 12:41:24 +0100

    update treesitter setup

--- init.lua
+++ init.lua
@@ -1061,25 +1061,34 @@ require('lazy').setup({
   },
   { -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
+    lazy = false, -- Cannot be lazy-loaded on main branch
     build = ':TSUpdate',
-    main = 'nvim-treesitter.configs', -- Sets main module to use for opts
-    -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
-    ---@module 'nvim-treesitter'
-    ---@type TSConfig
-    ---@diagnostic disable-next-line: missing-fields
-    opts = {
-      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
-      -- Autoinstall languages that are not installed
-      auto_install = true,
-      highlight = {
-        enable = true,
-        -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-        --  If you are experiencing weird indenting issues, add the language to
-        --  the list of additional_vim_regex_highlighting and disabled languages for indent.
-        additional_vim_regex_highlighting = { 'ruby' },
-      },
-      indent = { enable = true, disable = { 'ruby' } },
-    },
+    config = function()
+      -- Install core parsers on startup
+      local core_parsers = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' }
+      for _, parser in ipairs(core_parsers) do
+        vim.cmd('TSInstall! ' .. parser)
+      end
+      
+      -- Auto-install parser for any filetype if missing
+      vim.api.nvim_create_autocmd('FileType', {
+        callback = function()
+          local ft = vim.bo.filetype
+          if ft and ft ~= '' then
+            pcall(vim.cmd, 'TSInstall! ' .. ft)
+          end
+        end,
+      })
+      
+      -- Enable treesitter highlighting for all filetypes
+      vim.api.nvim_create_autocmd('FileType', {
+        pattern = '*',
+        callback = function()
+          local buf = vim.api.nvim_get_current_buf()
+          pcall(vim.treesitter.start, buf)
+        end,
+      })
+    end,
     -- There are additional nvim-treesitter modules that you can use to interact
     -- with nvim-treesitter. You should go explore a few and see what interests you:
     --
